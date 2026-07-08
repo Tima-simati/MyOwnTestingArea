e7b9ec737e1a7aca8b3cbf7eac1bc67ca0f2824b
Tima-simati <timaaks0ul@gmail.com>
2026-07-08 16:32:58 +0200

    Refactor number guessing game with constants
    to avoid magic numbers

--- NumberGuessingGame.cs
+++ NumberGuessingGame.cs
@@ -8,20 +8,15 @@ namespace Module2
         {
             Console.WriteLine("Welcome Challenger to the Number Guessing Game!");
             Console.WriteLine("I will now think of a number between 1 and 100.\nTry to guess the number.");
+            
+            //range for number guessing range
+            const int GUESSING_LOWERBOUND = 1;
+            const int GUESSING_UPPERBOUND = 100;
             //random number generated
             Random rng = new Random();
-            int randomNumber = rng.Next(1, 100); // Number to be guessed
+            int randomNumber = rng.Next(GUESSING_LOWERBOUND, GUESSING_UPPERBOUND); // Number to be guessed
             const int TRIES = 10; //how many times you can try to guess
-            //Console.WriteLine(randomNumber);
-
-            //trying approch with array of proxymity numbers around the randomNumber
-            /*int[] randomNumberCloseRange = new int[TRIES - 1];
-            for (int i = 0; i <= TRIES; i++)
-            {
-                randomNumberCloseRange[0] = randomNumber - 5 + i;
-                //Console.WriteLine(randomNumberCloseRange[0]);
-            }
-            */
+            const int CLOSE_GUESS = 5;
             //check for correct guess
             for (int i = 0; i < TRIES; i++)
             {
@@ -41,7 +36,7 @@ namespace Module2
                 }
                 Console.WriteLine($"You have {TRIES - 1 - i} guess(es) left.");
                 //Hint to Player, if he is only is 5 off
-                if (Math.Abs(randomNumber - numberGuessed) <= 5)
+                if (Math.Abs(randomNumber - numberGuessed) <= CLOSE_GUESS)
                 {
                     Console.WriteLine("You're close!");
                 }
