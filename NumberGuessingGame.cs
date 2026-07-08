aafa6c5fcb15373ccc70e523b2e42e9e5ba944a9
Tima-simati <timaaks0ul@gmail.com>
2026-07-08 09:59:55 +0200

    Add files via upload

---
+++ NumberGuessingGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Module2
+{
+    internal class NumberGuessingGame
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Challenger to the Number Guessing Game!");
+            Console.WriteLine("I will now think of a number between 1 and 100.\nTry to guess the number.");
+            //random number generated
+            Random rng = new Random();
+            int randomNumber = rng.Next(1, 100); // Number to be guessed
+            const int TRIES = 10; //how many times you can try to guess
+            //Console.Write(numberGuessed);
+
+            //check for correct guess
+            for (int i = 0; i < TRIES; i++)
+            {
+                int numberGuessed = Convert.ToInt16(Console.ReadLine());
+                if (numberGuessed == randomNumber)
+                {
+                    Console.WriteLine("Congratulations! You won!");
+                    return;
+                }
+                if (numberGuessed < randomNumber)
+                {
+                    Console.WriteLine("Too Low! Try again");
+                }
+                if (numberGuessed > randomNumber)
+                {
+                    Console.WriteLine("Too High! Try again");
+                }
+                Console.WriteLine($"You have {TRIES - 1 - i} guess(es) left.");
+                //Hint to Player, if he is only is 5 off
+                if(numberGuessed == randomNumber + 5 || numberGuessed == randomNumber - 5)
+                {
+                    Console.WriteLine("You're close!");
+                }
+            }
+        }
+    }
+}
