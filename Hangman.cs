46a0edb955a063583296c081858eb9544cad3c2f
Tima-simati <timaaks0ul@gmail.com>
2026-07-14 16:48:00 +0200

    every block was put in a while loop
    losing condition is working;
    winning condition is broken atm, will be analyzed

--- Hangman.cs
+++ Hangman.cs
@@ -15,50 +15,55 @@ namespace Hangman
             int wrongGuesses = 0;                //counter for wrong guesses
             int indexOfGuessedChar = 0;              //index of character in the word to be guessed
 
-
             Console.WriteLine("A word will now be picked. Try to guess the letters of the word.");
             Console.WriteLine("If you guess 6 times the letter wrong, you lose! Good luck.");
             Random rng = new Random();
             int randomIndex = rng.Next(START_OF_LIST, endOfList);
             string chosenWord = boxOfWords[randomIndex];
-            //write a block that helps you find out if a character is part of a string
-            Console.WriteLine("Your guess is: ");
-            Console.ReadKey();
-            string userInput = Convert.ToString(Console.ReadLine());
-            char userInputChar = userInput[0];
             string outputString = new string('_', chosenWord.Length); //parts of the chosenWord, which will be revealed
 
-            if (chosenWord.Contains(userInputChar))
+            while (wrongGuesses < WRONG_GUESSES_MAX)
             {
-                Console.WriteLine("You guessed a letter correct.");
-            }
-            else
-            {
-                wrongGuesses++;
-                int guessesLeft = WRONG_GUESSES_MAX - wrongGuesses;
-                Console.WriteLine($"Wrong guess! You have only {guessesLeft} guesses left.");
-            }
+                Console.WriteLine("Your guess is: ");
+                string userInput = Convert.ToString(Console.ReadLine());
 
+                char userInputChar = userInput[0];
 
-            //write some code to find the position of a specific letter in a word ( for loops seem like a good idea)
-            //write a block that outputs the current state of the game (like in the screenshot).
-            //Console.Clear() can help to make things look better
-            Console.Clear();
-            for (int i = 0; i < chosenWord.Length; i++)
-            {
-                if (chosenWord[i] == userInputChar)
+                if (chosenWord.Contains(userInputChar))
                 {
-                    indexOfGuessedChar = i;
-                    outputString = outputString.Insert(indexOfGuessedChar, userInputChar.ToString());
+                    //tracking of right guessing and winning condition
+                    Console.Clear();
+                    Console.WriteLine("You guessed a letter correct.");
+                    //Console.ReadKey();
+                    for (int i = 0; i < chosenWord.Length; i++)
+                    {
+                        if (chosenWord[i] == userInputChar)
+                        {
+                            indexOfGuessedChar = i;
+                            outputString = outputString.Insert(indexOfGuessedChar, userInputChar.ToString());
+                        }
+                    }
+                    Console.WriteLine(outputString);
                 }
-
-
+                else
+                {
+                    //tracking of wrong guesses and losing condition
+                    wrongGuesses++;
+                    int guessesLeft = WRONG_GUESSES_MAX - wrongGuesses;
+                    Console.WriteLine($"Wrong guess! You have only {guessesLeft} guesses left.");
+                }
+                if (outputString.Equals(chosenWord))
+                {
+                    Console.WriteLine("You did it. You guessed the word correctly! You win.");
+                    return;
+                }
+                if (wrongGuesses == WRONG_GUESSES_MAX)
+                {
+                    Console.WriteLine("You are out of guesses. You lose!");
+                    Console.WriteLine("Restart the game and try again!");
+                    return;
+                }
             }
-            Console.WriteLine(outputString);
-
-
-            // pack all of this in a loop, and keep track of tries
-            Console.WriteLine("This is the debugger working.");
         }
     }
 }
